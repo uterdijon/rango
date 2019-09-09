119683b9d9d4888eeade69666255d1a12e61bb41
Evan <evan.zeleznygreen@gmail.com>
2019-09-10 00:32:25 +0100

    Added urls and views

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hey there partner! <a href='http://127.0.0.1:8000/rango/about/'>Go to about page.</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='http://127.0.0.1:8000/'>Go to index page.</a>")
