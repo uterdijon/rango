4105acdb1caa11f887670b62e05a9447a91ce94c
Evan <evan.zeleznygreen@gmail.com>
2019-09-13 00:52:02 +0100

    completed index and about views with static and media files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='http://127.0.0.1:8000/rango/about/'>Go to about page.</a>")
+    context_dict = { 'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='http://127.0.0.1:8000/'>Go to index page.</a>")
+    context_dict = { 'boldmessage': 'This tutorial has been put together by Evan'}
+    return render(request, 'rango/about.html', context=context_dict)
