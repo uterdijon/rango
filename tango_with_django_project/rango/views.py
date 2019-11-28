be16a2d7d88682b53525a15ff1e390dca1e71615
Evan <evan.zeleznygreen@gmail.com>
2019-11-28 13:55:35 +0000

    added add category template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,5 @@
 from rango.models import Page
+from rango.forms import CategoryForm
 
 from django.shortcuts import render
 
@@ -34,4 +35,25 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context_dict)
 
 
+def add_category(request):
+    form = CategoryForm()
 
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+            # Now that the category is saved
+            # We could give a confirmation message
+            # But since the most recent category added is on the index page # Then we can direct the user back to the index page.
+            return index(request)
+        else:
+            # The supplied form contained errors -
+            # just print them to the terminal.
+            print(form.errors)
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', { 'form': form})
